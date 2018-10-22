94dcafc6ae0a5918201e92fda8cf9badae1796b2
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2018-10-22 17:59:22 +0100

    swapped rows and columns in loop

--- stencil.c
+++ stencil.c
@@ -52,11 +52,11 @@ int main(int argc, char *argv[]) {
 void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
   for (int j = 0; j < ny; ++j) {
     for (int i = 0; i < nx; ++i) {
-      tmp_image[j+i*ny] = image[j+i*ny] * 3.0/5.0;
-      if (i > 0)    tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.5/5.0;
-      if (i < nx-1) tmp_image[j+i*ny] += image[j  +(i+1)*ny] * 0.5/5.0;
-      if (j > 0)    tmp_image[j+i*ny] += image[j-1+i*ny] * 0.5/5.0;
-      if (j < ny-1) tmp_image[j+i*ny] += image[j+1+i*ny] * 0.5/5.0;
+      tmp_image[i+j*nx] = image[i+j*nx] * 3.0/5.0;
+      if (i > 0)    tmp_image[i+j*nx] += image[(i-1) + j*nx] * 0.5/5.0;
+      if (i < nx-1) tmp_image[i+j*nx] += image[(i+1) + j*nx] * 0.5/5.0;
+      if (j > 0)    tmp_image[i+j*nx] += image[i + (j-1)*nx] * 0.5/5.0;
+      if (j < ny-1) tmp_image[i+j*nx] += image[i + (j+1)*nx] * 0.5/5.0;
     }
   }
 }
